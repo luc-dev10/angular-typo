ed6f257e90f92b8376f4f280f5cf9dec88cb5f0a
luc-dev10 <luc.dev1008@gmail.com>
2022-01-31 00:12:49 -0500

    add app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typo-app';
+  title: string = 'typo';
+
+  // props
+  cardTitle: string = 'Typo';
+  cardText: string =
+    'Our platform enhances your typewriting skills. Start today to have no regrets tomorrow. ';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
